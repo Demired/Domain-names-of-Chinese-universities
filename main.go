9c0a3bf59db13e23f375227bd0308ba77f36058b
zhangyuan <nevermakeyoucrytoli@gmail.com>
2019-02-25 19:51:52 +0800

    解析代码

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type school struct {
+	URL  string `json:"url"`
+	Tag  string `json:"tag"`
+	Name string `json:"name"`
+}
+
+func main() {
+	f, err := os.Open("./new-edu.json")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	var schools []school
+	json.Unmarshal(b, &schools)
+	fmt.Printf("本库共收录%d所高校\n", len(schools))
+}

1f6311ec794091368ab8e5d1779b0b2ba4eea9f7
zhangyuan <nevermakeyoucrytoli@gmail.com>
2019-02-28 18:26:58 +0800

    edu

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 type school struct {
@@ -13,8 +15,17 @@ type school struct {
 	Name string `json:"name"`
 }
 
+var (
+	schools []school
+	jPath   = "./edu.json"
+	tPath   = "./edu.txt"
+	j2t     = kingpin.Flag("j2t", "install program").Bool()
+)
+
 func main() {
-	f, err := os.Open("./new-edu.json")
+	kingpin.Parse()
+
+	f, err := os.Open(jPath)
 	if err != nil {
 		panic(err)
 	}
@@ -23,7 +34,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	var schools []school
 	json.Unmarshal(b, &schools)
+	if *j2t {
+		json2txt()
+	}
+	for _, v := range schools {
+		fmt.Printf("Tag:%s	Name:%s	URL:%s \n", v.Tag, v.Name, v.URL)
+	}
 	fmt.Printf("本库共收录%d所高校\n", len(schools))
+}
+
+func json2txt() {
+	var str string
+	for _, v := range schools {
+		str = str + fmt.Sprintf("Tag:%s	Name:%s	URL:%s \n", v.Tag, v.Name, v.URL)
+	}
+	ioutil.WriteFile(tPath, []byte(str), 0644)
+	fmt.Println("写入完毕")
+	os.Exit(0)
 }
